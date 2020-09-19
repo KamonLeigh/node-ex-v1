ffb1efd7153ef86ea8392001d104e309fdb8bb72
Byron Dunkley <byron.dunkley@SQSMACLPT04.local>
2020-09-19 15:01:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressMongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+
+const app = express();
+
+// middleware logger for node 
+app.use(morgan('common'));
+
+// set headers in node app by default client is unable to tell that a node/express is running 
+app.use(helmet());
+
+// prevents client sending script to run in db
+app.use(expressMongoSanitize());
+
+
+module.exports = app;
